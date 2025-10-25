4b8494d33aadbd68983f451c1ad607632e0d69d3
Mathijs Schuurmans <mathijs.schuurmans@gmail.com>
2025-10-25 14:39:55 +0200

    Complete name change

--- src/omode/__init__.py
+++ src/omode/__init__.py
@@ -1,3 +1,7 @@
+"""Symbolic framework-agnostic optimization modeling in Python."""
+
+# ruff: noqa
+
 from .symbolic import get_framework, SymbolicFramework, SymbolContainer
 from .backends.casadi import CasadiModel, CASADI
 from .backends.cvxpy import CvxpyModel, CVXPY
--- src/test/test_basics.py
+++ src/test/test_basics.py
@@ -1,35 +1,36 @@
 import pytest
 
 
-def test_import_optimo():
-    import optimo
+def test_import_omode():
+    import omode
 
 
 def test_import_cvxpy():
-    from optimo import CvxpyModel
+    from omode import CvxpyModel
 
 
 def test_import_casadi():
-    from optimo import CasadiModel
+    from omode import CasadiModel
 
 
 # def test_create_model():
-#     from optimo import CVXPY, get_framework
+#     from omode import CVXPY, get_framework
 #     modeler = get_framework(CVXPY)
 
 
 # def test_create_model_explicit():
-#     from optimo import CvxpyModel
+#     from omode import CvxpyModel
 #     # import cvxpy as cp
 #     modeler = CvxpyModel()
 #     var = modeler.new_decision_var("test", (1,))
 
 
 def test_create_unimplemented_model():
-    import optimo as om
-    from optimo.symbolic import NoSuchFrameworkException
+    import omode as om
+    from omode.symbolic import NoSuchFrameworkException
+
     with pytest.raises(NoSuchFrameworkException):
-        om.get_framework("something stupid")
+        om.get_framework("Something nonexistent")
 
 
 if __name__ == "__main__":
